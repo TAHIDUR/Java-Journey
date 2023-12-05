64c47c7ccf2caca42b3201009e0cee8e7ab96eb2
tahidur <tahidurrahman.tuhin@gmail.com>
2023-12-05 11:35:08 +0600

    java practice start

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // variables
+
+        int age = 10;
+        double shoeSize = 9.5;
+
+        System.out.println(' '+ age);
+        System.out.println("My age: "+age+" and shoe size: "+shoeSize);
+        System.out.printf("My age: %d and shoe size: %.2f", age, shoeSize);
+    }
+}
