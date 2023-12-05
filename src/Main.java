fb0e4bfe113717c3235476170a01c47561290be6
tahidur <tahidurrahman.tuhin@gmail.com>
2023-12-05 19:16:44 +0600

    Java initial practice

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,65 @@
+import classes.Messages;
+
 public class Main {
     public static void main(String[] args) {
-        // variables
+        // call a static method from a class
+        Messages.welcomeMessage();
 
+        // calling method using object
+        Messages messages = new Messages();
+        // set value to a variable in Messages class using object
+        messages.name = "Tuhin";
+
+        messages.welcome();
+
+        variables();
+        loops();
+    }
+
+    private static void variables() {
+        // primitive types
+        // starts with lowercase
         int age = 10;
         double shoeSize = 9.5;
+        char empty = ' ';
 
-        System.out.println(' '+ age);
+        // non-primitive types
+        // starts with uppercase
+        String name = "Tuhin";
+
+        System.out.println(empty + age); // ascii of empty string (' ') is 32
         System.out.println("My age: "+age+" and shoe size: "+shoeSize);
-        System.out.printf("My age: %d and shoe size: %.2f", age, shoeSize);
+        System.out.printf("My age: %d and shoe size: %.2f\n", age, shoeSize);
+        System.out.printf("My name is %s\n", name);
+
+        methodWithParameter(age);
+    }
+
+    private static void methodWithParameter(int age) {
+
+        String result = methodWithReturn(age);
+
+        System.out.println(result);
+    }
+
+    private static String methodWithReturn(int age) {
+        String result;
+
+        // conditional statement
+        if (age > 18) {
+            result = "I am adult";
+        } else if (age <= 0) {
+            result = "invalid age";
+        } else{
+            result = "I am not adult";
+        }
+        return result;
+    }
+
+    private static void loops() {
+        System.out.println("Tabulation sheet for 8");
+        for(int i = 1; i < 11; i++) {
+            System.out.printf("%d x 8 = %d\n", i, i*8);
+        }
     }
 }
---
+++ src/classes/Messages.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Messages {
+    public String name;
+    public static void welcomeMessage() {
+        System.out.println("Welcome to Java");
+    }
+
+    public void welcome() {
+        System.out.println("Hello "+name);
+    }
+}
