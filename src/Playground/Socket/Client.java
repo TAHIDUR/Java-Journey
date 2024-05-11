f043749c643288d241a6c8fa3bf8430e0f50cf7a
tahidur <tahidurrahman.tuhin@gmail.com>
2024-05-11 20:37:10 +0600

    Message from client to server socket

---
+++ src/Playground/Socket/Client.java
@@ -0,0 +1,25 @@
+package Playground.Socket;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static Utilities.Helper.println;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 9999)) {
+            println("Client started");
+            // User input taken
+            Scanner scanner = new Scanner(System.in);
+            String message = scanner.nextLine();
+
+            // sent message to server
+            OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+            writer.write(message);
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Playground/Socket/Server.java
@@ -0,0 +1,22 @@
+package Playground.Socket;
+
+import static Utilities.Helper.println;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        println("Server started......");
+        try (ServerSocket serverSocket = new ServerSocket(9999)) {
+            println("Waiting for client .............");
+            Socket socket = serverSocket.accept();
+            println("Connection established");
+
+            // Get client message from input stream
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = reader.readLine();
+            println(message);
+        }
+    }
+}
