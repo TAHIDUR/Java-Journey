f86c9ff44632d4a742ecdb6585f948d1b022ae66
tahidur <tahidurrahman.tuhin@gmail.com>
2024-05-14 22:10:16 +0600

    Chatting using TCP and multithreading

--- src/Playground/Socket/Tcp/Chat/Client.java
+++ src/Playground/Socket/Tcp/Chat/Client.java
@@ -1,20 +1,83 @@
 package Playground.Socket.Tcp.Chat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 9999)) {
-            while (true) {
-                Scanner scanner = new Scanner(System.in);
-                String message = scanner.nextLine();
+import static Utilities.Helper.print;
 
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println(message);
+public class Client {
+
+    private Socket socket;
+    private PrintWriter printWriter;
+    private BufferedReader bufferedReader;
+    private String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.username = username;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.printWriter = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            closeConnection(socket, bufferedReader, printWriter);
+        }
+    }
+
+
+    public void sendMessage() {
+        printWriter.println(username);
+        Scanner scanner = new Scanner(System.in);
+        while (socket.isConnected()) {
+            String chatMessage = scanner.nextLine();
+            printWriter.println(chatMessage);
+        }
+    }
+
+
+    private void closeConnection(Socket socket, BufferedReader bufferedReader, PrintWriter printWriter) {
+        try {
+            if (socket != null) {
+                socket.close();
             }
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        } catch (IOException ignore) {
+        }
+    }
+
+
+    public void listenMessages() {
+        new Thread(() -> {
+            String chat;
+            while (socket.isConnected()) {
+                try {
+                    chat = bufferedReader.readLine();
+                    System.out.println(chat);
+                } catch (IOException e) {
+                    closeConnection(socket, bufferedReader, printWriter);
+                }
+            }
+        }).start();
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            print("Please enter your username: ");
+            Scanner scanner = new Scanner(System.in);
+            String username = scanner.nextLine();
+            Socket socket = new Socket("localhost", 9999);
+            Client client = new Client(socket, username);
+            client.listenMessages();
+            client.sendMessage();
         } catch (IOException ignored) {
         }
     }
--- src/Playground/Socket/Tcp/Chat/Server.java
+++ src/Playground/Socket/Tcp/Chat/Server.java
@@ -3,26 +3,103 @@ package Playground.Socket.Tcp.Chat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import static Utilities.Helper.println;
 
 public class Server {
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(9999)) {
-            println("Server started");
-            Socket socket = serverSocket.accept();
-            println("Client joined");
-            String message;
-            while (true) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                while ((message = in.readLine()) != null) {
-                    println(message);
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                println("Client joined");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    private void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException ignore) {
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9999);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
+    private static class ClientHandler implements Runnable {
+        private static final ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+        private Socket socket;
+        private BufferedReader bufferedReader;
+        private PrintWriter printWriter;
+        private String username;
+
+        public ClientHandler(Socket socket) {
+            try {
+                this.socket = socket;
+                this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.printWriter = new PrintWriter(socket.getOutputStream(), true);
+                this.username = bufferedReader.readLine();
+                clientHandlers.add(this);
+                broadcastMessage("Server: " + username + " joined");
+            } catch (IOException e) {
+                closeConnection(socket, bufferedReader, printWriter);
+            }
+        }
+
+        private void broadcastMessage(String message) {
+            for (ClientHandler clientHandler : clientHandlers) {
+                if (!this.equals(clientHandler))
+                    clientHandler.printWriter.println(message);
+            }
+        }
+
+
+        private void closeConnection(Socket socket, BufferedReader bufferedReader, PrintWriter printWriter) {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (printWriter != null) {
+                    printWriter.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void run() {
+            String clientMessage;
+            while (socket.isConnected()) {
+                try {
+                    clientMessage = bufferedReader.readLine();
+                    broadcastMessage(username + ": " + clientMessage);
+                } catch (IOException e) {
+                    closeConnection(socket, bufferedReader, printWriter);
                 }
             }
-        } catch (IOException ignored) {
-
         }
     }
 }
