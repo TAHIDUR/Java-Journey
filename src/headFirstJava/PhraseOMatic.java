6361c03b5483f086a135b883ec3db87da3d49ebf
TAHIDUR <tahidur@veefin.com>
2025-10-29 18:53:49 +0600

    Head First Java

---
+++ src/headFirstJava/PhraseOMatic.java
@@ -0,0 +1,13 @@
+package headFirstJava;
+
+import java.util.Random;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int x = rand.nextInt(5);
+
+        System.out.println(x);
+    }
+}
